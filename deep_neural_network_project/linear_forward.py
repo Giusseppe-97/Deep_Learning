8d944ead46be94a554838347b4b233b2c8b25f5e
Giusseppe-97 <josepablonavarro97@gmail.com>
2022-03-09 13:46:40 -0500

    Update linear_forward.py
    Implement the forward propagation for the LINEAR->ACTIVATION layer
    Missing RELU and SIGMOID functions

--- deep_neural_network_project/linear_forward.py
+++ deep_neural_network_project/linear_forward.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import relu
+import sigmoid
 
 # GRADED FUNCTION: linear_forward
 
@@ -18,8 +20,43 @@ def linear_forward(A, W, b):
     cache -- a python tuple containing "A", "W" and "b" ; stored for computing the backward pass efficiently
     """
 
-    Z = np.dot(W,A) + b
+    Z = np.dot(W, A) + b
 
     cache = (A, W, b)
 
     return Z, cache
+
+# GRADED FUNCTION: linear_activation_forward
+
+
+def linear_activation_forward(A_prev, W, b, activation):
+    """
+    Implement the forward propagation for the LINEAR->ACTIVATION layer
+
+    Arguments:
+    A_prev -- activations from previous layer (or input data): (size of previous layer, number of examples)
+    W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
+    b -- bias vector, numpy array of shape (size of the current layer, 1)
+    activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
+
+    Returns:
+    A -- the output of the activation function, also called the post-activation value 
+    cache -- a python tuple containing "linear_cache" and "activation_cache";
+             stored for computing the backward pass efficiently
+    """
+
+    if activation == "sigmoid":
+
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = sigmoid(Z)
+
+        # YOUR CODE ENDS HERE
+
+    elif activation == "relu":
+
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = relu(Z)
+
+    cache = (linear_cache, activation_cache)
+
+    return A, cache
