e17c0744ca31555e9607f7ff3a1e4afc6b0b5699
Giusseppe-97 <josepablonavarro97@gmail.com>
2022-03-10 11:20:04 -0500

    Update linear_backward.py
    Implement the backward propagation for the LINEAR->ACTIVATION layer.

--- deep_neural_network_project/linear_backward.py
+++ deep_neural_network_project/linear_backward.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import relu_backward
+import sigmoid_backward
 
 # GRADED FUNCTION: linear_backward
 
@@ -24,5 +26,46 @@ def linear_backward(dZ, cache):
     db = (1/m)*np.sum(dZ, axis=1, keepdims=True)
     dA_prev = np.dot(W.T,dZ)
     
+    
+
+    return dA_prev, dW, db
+
+# GRADED FUNCTION: linear_activation_backward
+
+def linear_activation_backward(dA, cache, activation):
+    """
+    Implement the backward propagation for the LINEAR->ACTIVATION layer.
+    
+    Arguments:
+    dA -- post-activation gradient for current layer l 
+    cache -- tuple of values (linear_cache, activation_cache) we store for computing backward propagation efficiently
+    activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
+    
+    Returns:
+    dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
+    dW -- Gradient of the cost with respect to W (current layer l), same shape as W
+    db -- Gradient of the cost with respect to b (current layer l), same shape as b
+    """
+    linear_cache, activation_cache = cache
+    
+    if activation == "relu":
+        #(≈ 2 lines of code)
+        # dZ =  ...
+        # dA_prev, dW, db =  ...
+        # YOUR CODE STARTS HERE
+        dZ = relu_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+        
+        # YOUR CODE ENDS HERE
+        
+    elif activation == "sigmoid":
+        #(≈ 2 lines of code)
+        # dZ =  ...
+        # dA_prev, dW, db =  ...
+        # YOUR CODE STARTS HERE
+        dZ = sigmoid_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+        
+        # YOUR CODE ENDS HERE
     
     return dA_prev, dW, db
